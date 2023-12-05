e7f5cb55568db200c1a21759d1f502120fa5e009
Daniela Gutperl <daniela@Danielas-MacBook-Pro.local>
2023-12-05 11:10:35 +0000

    test 4 main App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          G'dayzzz Mate
+          G'dayzzz Mates
         </p>
         <a
           className="App-link"
